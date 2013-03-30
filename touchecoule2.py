d85057d3f38bfa2226e02902f8977aaa2232d7b9
Léo Moulin <leomouli@ulb.ac.be>
2013-03-30 21:35:57 +0100

    Modifs

--- touchecoule2.py
+++ touchecoule2.py
@@ -5,7 +5,18 @@ from tkinter import *
 
 #----------------------------------------entrees joueur        OK
 
-def transforme_joueur_ordi(tir):                                        #J5 => [9, 4]
+def transforme_joueur_ordi(tir): 
+	"""
+	Transforme une entrée du joueur en une liste de 2 nombres plus facile à manipuler.
+	
+	Arguments:
+		tir(str): coordonnées rentrées par le joueur							
+	Valeurs de retour:
+		case(list): liste de 2 nombres 
+	Exemple:
+		print(transforme_joueur_ordi(J5))
+		>>>[9,4]
+	"""							
 	dico = {"a":0,"b":1,"c":2,"d":3,"e":4,"f":5,"g":6,"h":7,"i":8,"j":9}
 	case = [dico[str.lower(tir[0])]]
 	y=""
@@ -14,11 +25,33 @@ def transforme_joueur_ordi(tir):                                        #J5 => [
 	case.append(int(y)-1)
 	return case
 	
-def transforme_ordi_joueur(case):                                        #[9, 4] => J5
+def transforme_ordi_joueur(case): 
+	"""
+	Transforme une liste de 2 nombres par une case sur la grille.
+	
+	Arguments:
+		case(list): liste de 2 nombres
+		
+	Valeurs de retour:
+		tir(str): coordonnées entrées par le joueur
+		
+	Exemple:
+		print(transforme_ordi_joueur([9,4]))
+		>>>J5
+	"""									#[9, 4] => J5
 	dico = ["A","B","C","D","E","F","G","H","I","J"]
 	return dico[case[0]] + str(case[1]+1)
 	
 def verif_si_entree_possible(entree, tableau):
+	"""
+	
+	Arguments:
+		
+	Valeurs de retour:
+		
+	Exemple:
+		
+	"""	
 	dico = ["A","B","C","D","E","F","G","H","I","J"]
 	nombre = ""
 	result = True
@@ -40,13 +73,38 @@ def verif_si_entree_possible(entree, tableau):
 	return result
 	
 #----------------------------------------lit fichier navires.dat        OK
-def lit_fichier():                    #['porte-avions 1 5\n', 'cuirasse 1 4\n', 'sous-marin 1 3\n', 'destroyer 1 2 \n']
+def lit_fichier(): 
+	"""	
+	Lit le fichier "navires.dat" et le convertit en une liste du type 
+	['porte-avions 1 5\n', 'cuirasse 1 4\n', 'sous-marin 1 3\n', 'destroyer 1 2 \n']
+	
+	Arguments:
+		Aucun
+		
+	Valeurs de retour:
+		contenu(list): liste contenant le nom, le nombre et la taille des différents navires
+	"""                             
 	fichier = open("navires.dat", "r")
 	contenu = fichier.readlines()
 	fichier.close()
 	return contenu
 	
-def liste_fichier():                  #[['porte-avions', 1, 5], ['cuirasse', 1, 4], ['sous-marin', 1, 3], ['destroyer', 1, 2]]
+def liste_fichier():    
+	"""
+	Réarrange la liste renvoyée par la fonction lit_fichier() en une liste de listes du type
+	[['porte-avions', 1, 5], ['cuirasse', 1, 4], ['sous-marin', 1, 3], ['destroyer', 1, 2]], 
+	la rendant plus facile à manipuler par la suite.
+	
+	Arguments:
+		Aucun
+		
+	Valeurs de retour:
+		contenu(list): liste de listes contenant le nom, le nombre et la taille des différents navires
+		
+	Exemple:
+		print(liste_fichier())
+		>>> [['porte-avions', 1, 5], ['cuirasse', 1, 4], ['sous-marin', 1, 3], ['destroyer', 1, 2]]
+	"""
 	contenu = lit_fichier() 
 	    
 	for i in range(len(contenu)):
@@ -69,6 +127,15 @@ def liste_fichier():                  #[['porte-avions', 1, 5], ['cuirasse', 1, 
 #---------------------------------------interface graphique avec Tkinter        peut etre art ascii
 
 def art_ascii(tableau):
+	"""
+	
+	Arguments:
+		
+	Valeurs de retour:
+		
+	Exemple:
+		
+	"""	
 	dico = ["A","B","C","D","E","F","G","H","I","J"]
 	print("  ", end="")
 	for k in range(len(tableau)):
@@ -94,6 +161,16 @@ def art_ascii(tableau):
 		print("|")
 		
 def art_ascii2(tableau):
+	"""
+	
+	
+	Arguments:
+		
+	Valeurs de retour:
+		
+	Exemple:
+		
+	"""	
 	dico = ["A","B","C","D","E","F","G","H","I","J"]
 	print("  ", end="")
 	for k in range(len(tableau)):
@@ -122,13 +199,31 @@ def art_ascii2(tableau):
 #--------------------------------------tir joueur      OK
 
 def verif_si_deja_tire(tir, tableau_IA):
+	"""
+	
+	Arguments:
+		
+	Valeurs de retour:
+		
+	Exemple:
+		
+	"""	
 	if tableau[tir[0]][tir[1]][1] == False:
 		return True
 	else:
 		print("Vous avez déjâ tiré sur cette case\nVeuillez recommencer")
 		return False
 
-def tir_joueur(tableau_IA, navires, tirs_precedent):       
+def tir_joueur(tableau_IA, navires, tirs_precedent): 
+	"""
+	
+	Arguments:
+		
+	Valeurs de retour:
+		
+	Exemple:
+		
+	"""	
 	tir_possible = False
 	
 	while not(tir_possible):
@@ -148,12 +243,30 @@ def tir_joueur(tableau_IA, navires, tirs_precedent):
 #--------------------------------------tir IA
 
 def cree_liste_100_cases():
+	"""
+	Crée une liste contenant les 100 premiers nombres.
+	
+	Arguments:
+		Aucun
+		
+	Valeurs de retour:
+		l(list): liste contenant les nombres de 0 à 99
+	"""
 	l = []
 	for i in range(100):
 		l.append(i)
 	return l
 	
 def tir_aleatoire(tableau_joueur, liste_tir, tir):
+	"""
+	
+	Arguments:
+		
+	Valeurs de retour:
+		
+	Exemple:
+		
+	"""	
 	tir_valide = False
 	i=0
 	touche = False
@@ -177,6 +290,15 @@ def tir_aleatoire(tableau_joueur, liste_tir, tir):
 	return touche
 	
 def tir_semi_aleatoire(tableau_joueur, tirs_precedents, tir):
+	"""
+	
+	Arguments:
+		
+	Valeurs de retour:
+		
+	Exemple:
+		
+	"""	
 	tir_valide = False
 	while not(tir_valide):
 		tir_valide = True
@@ -230,11 +352,30 @@ def tir_semi_aleatoire(tableau_joueur, tirs_precedents, tir):
 		return False
 	
 def tir_non_aleatoire1(tirs_precedent, tableau_joueur, tir):
+	"""
+	
+	
+	Arguments:
+		
+	Valeurs de retour:
+		
+	Exemple:
+		
+	"""	
 	if tirs_precedent[len(tirs_precedent)-1]:
 		a=0
 	
 	
-def tir_IA(tableau_joueur, tirs_precedents):      #ex tirs_precedent: [[1,-1,[2,5]],[2,0,[3,6]]] [orientaton(-1 si aléatoire)[x,y]]  
+def tir_IA(tableau_joueur, tirs_precedents): 
+	"""
+	
+	Arguments:
+		
+	Valeurs de retour:
+		
+	Exemple:
+		
+	"""	     #ex tirs_precedent: [[1,-1,[2,5]],[2,0,[3,6]]] [orientaton(-1 si aléatoire)[x,y]]  
 	liste_tir = cree_liste_100_cases()
 	shuffle(liste_tir)
 	tir = [1,-1,[-1,-1]]
@@ -261,6 +402,15 @@ def tir_IA(tableau_joueur, tirs_precedents):      #ex tirs_precedent: [[1,-1,[2,
 #----------------------------------------phase de tir
 
 def phase_de_tir(navires,tableau_IA,tableau_joueur):
+	"""
+	
+	Arguments:
+		
+	Valeurs de retour:
+		
+	Exemple:
+		
+	"""	
 	fin_partie = False
 	tirs_precedent_joueur = []
 	tirs_precedent_IA = [[1,-1,[-1,-1]]]   #[orientation, x, y] si veux tir aléatoire orientation = -1
@@ -278,6 +428,15 @@ def phase_de_tir(navires,tableau_IA,tableau_joueur):
 #--------------------------------------placement navire joueur (doit etre polyvalent si multijoueur)
 
 def demande_case_et_msgerreur(navire):
+	"""
+	
+	Arguments:
+		
+	Valeurs de retour:
+		
+	Exemple:
+		
+	"""	
 	bonne_entree = False
 	while not bonne_entree:
 		bonne_entree = False
@@ -306,7 +465,14 @@ def demande_case_et_msgerreur(navire):
 	
 def determine_orientation_joueur(x,y,navire, length):
 	"""
-	"""
+	
+	Arguments:
+		
+	Valeurs de retour:
+		
+	Exemple:
+		
+	"""	
 
 	if x > length-navire[2] and y < length-navire[2]:
 		orientation = -1
@@ -320,6 +486,15 @@ def determine_orientation_joueur(x,y,navire, length):
 
 
 def placement_navires_joueur(tableau_joueur, navires):
+	"""
+	
+	Arguments:
+		
+	Valeurs de retour:
+		
+	Exemple:
+		
+	"""	
 	dico = {"a":0,"b":1,"c":2,"d":3,"e":4,"f":5,"g":6,"h":7,"i":8,"j":9}
 	length = len(tableau_joueur)
 	for navire in navires:
@@ -359,6 +534,15 @@ def placement_navires_joueur(tableau_joueur, navires):
 	return tableau_joueur
 	
 def demande_orientation(orientation,tableau_joueur,navire, x, y):
+	"""
+	
+	Arguments:
+		
+	Valeurs de retour:
+		
+	Exemple:
+		
+	"""	
 	if orientation == "choix":
 		orientation=int(input("Choisissez l'orientation de votre "+navire[0]+": 1 pour haut, 0 pour droite "))
 		if orientation == 0:
@@ -385,6 +569,15 @@ def demande_orientation(orientation,tableau_joueur,navire, x, y):
 #--------------------------------------placement navire IA         OK
 
 def determine_orientation(tableau_IA, navire, x, y):
+	"""
+	
+	Arguments:
+		
+	Valeurs de retour:
+		
+	Exemple:
+		
+	"""	
 	if x > len(tableau_IA)-navire[2] and y > len(tableau_IA)-navire[2]:
 		orientation = -1
 	elif y > len(tableau_IA)-navire[2]:
@@ -396,6 +589,15 @@ def determine_orientation(tableau_IA, navire, x, y):
 	return orientation
 	
 def verif_si_peut_placer(tableau_IA, longueur_navire, orientation, x, y):
+	"""
+	
+	Arguments:
+		
+	Valeurs de retour:
+		
+	Exemple:
+		
+	"""	
 	verif = True
 	
 	for i in range(longueur_navire):
@@ -411,6 +613,15 @@ def verif_si_peut_placer(tableau_IA, longueur_navire, orientation, x, y):
 	return verif
 	
 def place_navire(tableau_IA, navire, orientation, x, y):
+	"""
+	
+	Arguments:
+		
+	Valeurs de retour:
+		
+	Exemple:
+		
+	"""	
 	for i in range(navire[2]):
 		if orientation == 0:
 			tableau_IA[x+i][y][0] = navire[0]
@@ -418,6 +629,15 @@ def place_navire(tableau_IA, navire, orientation, x, y):
 			tableau_IA[x][y+i][0] = navire[0]
 			
 def placement_navires_IA(tableau_IA, navires):
+	"""
+	
+	Arguments:
+		
+	Valeurs de retour:
+		
+	Exemple:
+		
+	"""	
 	for i in range(len(navires)):
 		for j in range(navires[i][1]):
 			bien_place = False
@@ -440,7 +660,16 @@ def placement_navires_IA(tableau_IA, navires):
 
 
 #---------------------------------------partie principale
-def cree_tableau():       #crée un tableau vide 10*10 contenant des ["",False]
+def cree_tableau():
+	"""
+	Crée un tableau vide 10x10 contenant des ["",False]
+	
+	Arguments:
+		Aucun
+		
+	Valeurs de retour:
+		tableau(list): liste de listes contenant des ["",False]
+	"""
 	tableau = []
 	for i in range(10):
 		tableau.append([])
@@ -449,6 +678,15 @@ def cree_tableau():       #crée un tableau vide 10*10 contenant des ["",False]
 	return tableau
 
 def lance_jeu(navires):
+	"""
+	
+	Arguments:
+		
+	Valeurs de retour:
+		
+	Exemple:
+		
+	"""	
 	tableau_IA = cree_tableau()                                              #création des tableaux OK
 	tableau_joueur = cree_tableau()                                          # OK
 	
@@ -461,6 +699,15 @@ def lance_jeu(navires):
 	
 
 def touche_coule():
+	"""
+	
+	Arguments:
+		
+	Valeurs de retour:
+		
+	Exemple:
+		
+	"""	
 	rejouer = "o"
 	
 	while rejouer == "o":
