801e42e357a9d88f99b892cc64c710fe28b96ccb
etnarek <romain-fontaine@outlook.com>
2013-03-30 13:27:06 +0100

    initialisation

--- touchecoule.py
+++ touchecoule.py
@@ -1,15 +1,43 @@
 from random import randint
 from random import shuffle
+from tkinter import *
 
-dico={"a":0,"b":1,"c":2,"d":3,"e":4,"f":5,"g":6,"h":7,"i":8,"j":9}
 
+#----------------------------------------entrees joueur
 
+def transforme_joueur_ordi(tir):                                        #J5 => [9, 4]
+	dico = {"a":0,"b":1,"c":2,"d":3,"e":4,"f":5,"g":6,"h":7,"i":8,"j":9}
+	case = [dico[str.lower(tir[0])]]
+	y=""
+	for i in range(len(tir)-1):
+		y = y+tir[i+1]
+	case.append(int(y)-1)
+	return case
+	
+def transforme_ordi_joueur(case):                                        #[9, 4] => J5
+	dico = ["A","B","C","D","E","F","G","H","I","J"]
+	return dico[case[0]] + str(case[1]+1)
+	
+def verif_si_entree_possible(entree):
+	dico = ["A","B","C","D","E","F","G","H","I","J"]
+	nombre = ""
+	result = True
+	
+	if not(str.upper(entree[0]) in dico):
+		result = False
+		
+	for i in range(len(entree)-1):
+		nombre = nombre + entree[i+1]
+		if not(entree[i+1].isnumeric()):
+			result = False
+			
+	if result != False and (int(nombre)>10 or int(nombre)<1):
+		result = False
+	return result
+	
 #----------------------------------------lit fichier navires.dat
-def lit_fichier():
-	"""
-	Lit le fichier "navires.dat", et 
-	"""
-	fichier = open("navires.dat", "r")     #['porte-avions 1 5\n', 'cuirasse 1 4\n', 'sous-marin 1 3\n', 'destroyer 1 2 \n']
+def lit_fichier():                    #['porte-avions 1 5\n', 'cuirasse 1 4\n', 'sous-marin 1 3\n', 'destroyer 1 2 \n']
+	fichier = open("navires.dat", "r")
 	contenu = fichier.readlines()
 	fichier.close()
 	return contenu
@@ -42,6 +70,19 @@ def liste_fichier():                  #[['porte-avions', 1, 5], ['cuirasse', 1, 
 
 #--------------------------------------tir joueur
 
+def verif_tir_joueur(tir, tableau_IA):
+	if tir[0]<0:
+		a=0
+
+def tir_joueur(tableau_IA, navires, tirs_precedent):
+	tir_possible = False
+	
+	while not(tir_possible):
+		tir = transforme_joueur_ordi(str(input("Entrez votre tir (ex:J7): ")))
+		tir_possible = verif_tir_joueur(tir, tableau_IA)
+	tirs_precedent.append(tir)
+		
+
 
 #--------------------------------------tir IA
 
@@ -50,25 +91,30 @@ def cree_liste_100_cases():
 	for i in range(100):
 		l.append(i)
 	return l
-
-def tir_IA(tableau_joueur):
+	
+def tir_aleatoire(tableau_joueur, liste_tir):
+	case = liste_tir.pop(0)
+	x = case // 10
+	y = case % 10
+	if tableau_joueur[x][y][1] == True:
+		tir_valide = False
+	else:
+		tableau_joueur[x][y][1] = True
+		tir_valide = True
+	print("")
+	print(x,y)
+	return tir_valide
+	
+def tir_IA(tableau_joueur, tir_precedent):    
 	liste_tir = cree_liste_100_cases()
 	shuffle(liste_tir)
 	tir_valide = False
 	
 	while not(tir_valide):
-		case = liste_tir.pop(0)
-		x = case // 10
-		y = case % 10
-		
-		if tableau_joueur[x][y][1] == True:
-			pass
-		else:
-			tableau_joueur[x][y][1] = True
-			tir_valide = True
+		if tir_precedent[0] == -1:
+			tir_valide = tir_aleatoire(tableau_joueur, liste_tir)
 			
-	print("")     #peut etre renvoyer les coor x et y pour la verif de tir
-	print(x,y)
+	     #peut etre renvoyer les coor x et y pour la verif de tir
 		
 	
 	
@@ -77,26 +123,42 @@ def tir_IA(tableau_joueur):
 
 def phase_de_tir(navires,tableau_IA,tableau_joueur):
 	fin_partie = False
-	
-	while not(fin_partie):                       #une variable qui compte le nombre de navire détruits chez chacun ?????
-		tir_joueur(tableau_IA, navires)
-		tir_IA(tableau_joueur)
+	tirs_precedent_joueur = []
+	tir_precedent_IA = [-1,0,0]   #[orientation, x, y] si veux tir aléatoire orientation = -1
+	while not(fin_partie):                       #une variable qui compte le nombre de navire détruit chez chacun ?????
+		touche_joueur = tir_joueur(tableau_IA, navires, tirs_precedent_joueur)
+		tir_IA(tableau_joueur, tir_precedent_IA)
+		
 		fin_partie = verif_victoire(tableau_IA, tableau_joueur)
 
 
 #--------------------------------------placement navire joueur (doit etre polyvalent si multijoueur)
-def demande_case_et_msgerreur():
+
+def demande_case_et_msgerreur(navire):
 	bonne_entree = False
 	while not bonne_entree:
-		case=str(input("A quelle case voulez-vous placer votre",navires[i][0],"? : (ex:H6)"))
-		if case[0].isalpha(): 
-			bonne_entree=True
-						
-		for l in range(len(case)-1):
-			if not(case[l+1].isnumeric()):
-				bonne_entree = False
-		if bonne_entree == False :
-			print("Cette case n'existe pas. Recommencez")
+		bonne_entree = False
+		case=str(input("A quelle case voulez-vous placer votre"+navire+"? : (ex:H6)"))
+		if len(case) == 2:
+			if 'a' <= case[0] <= 'j':
+				if '1' <= case[1] <= '9':
+					bonne_entree = True
+				else:
+					print("Le deuxiemme nombre doit etre un chiffre entre 1 et 10.")
+			else:
+				print("Il faut une lettre en première position entre a et j.")
+		elif len(case)==3:
+			if 'a' <= case[0] <= 'j':
+				if case[1:2] == '10':
+					bonne_entree = True
+				else:
+					print("Le deuxiemme nombre doit etre un chiffre entre 1 et 10.")
+			else:
+				print("Il faut une lettre en première position entre a et j.")
+		else:
+			print("Ce n'est pas le bon nombre de paramètre")
+				
+
 	return case
 	
 def determine_orientation_joueur(x,y,navires):
@@ -169,9 +231,6 @@ def demande_orientation(orientation,bonne_orientation,tableau_joueur,navires):
 			bonne_orientation=False
 
 		return tableau_joueur
-	
-placement_navires_joueur()
-	
 #--------------------------------------placement navire IA
 
 def determine_orientation(tableau_IA, navire, x, y):
@@ -258,10 +317,16 @@ def touche_coule():
 		lance_jeu(navires)
 		rejouer = str(input("Voulez-vous rejouer ? (o/n) : "))
 	return 0
-	
+"""	
 tableau = cree_tableau()
 placement_navires_IA(tableau, liste_fichier())
-tir_IA(tableau)
+tir_IA(tableau, [-1,0,0])
+print(tableau)
+"""
+"""
+tableau = cree_tableau()
+placement_navires_joueur(tableau, liste_fichier())
 print(tableau)
 
+"""
 
